aad86731ace053293a6e47a7f440c5e31803dd96
Tim <tgorsuch@redventures.com>
2021-02-27 16:05:10 -0500

    adds customizeing gernerated html file

--- webpack.config.js
+++ webpack.config.js
@@ -54,6 +54,12 @@ module.exports = {
       filename: "styles.[contenthash].css",
     }),
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Hello World",
+      filename: "subfolder/custom_filename.html",
+      meta: {
+        description: "Some kind of info for description",
+      },
+    }),
   ],
 };
